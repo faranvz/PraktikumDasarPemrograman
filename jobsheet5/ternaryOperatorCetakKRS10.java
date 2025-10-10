1f2659e4dbe675d3e772a7141fce2928a41e97cc
faranvz <s4tgo77@gmail.com>
2025-10-10 11:04:46 +0700

    Tugas 2

---
+++ jobsheet5/ternaryOperatorCetakKRS10.java
@@ -0,0 +1,19 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ternaryOperatorCetakKRS10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan sebuah bilangan bulat: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+    }
+    }
+
