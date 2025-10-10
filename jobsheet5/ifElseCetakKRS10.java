931469750c48dd83acc21ef6d90c81e96cdd7c44
faranvz <s4tgo77@gmail.com>
2025-10-10 09:21:21 +0700

    Modifikasi Percobaan 2

--- jobsheet5/ifElseCetakKRS10.java
+++ jobsheet5/ifElseCetakKRS10.java
@@ -5,16 +5,15 @@ public class ifElseCetakKRS10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.println("Apakah UKT sudah lunas? (true/flase): ");
+        System.out.println("Masukkan semester: ");
         int semester = sc.nextInt();
-
-if (semester == 1) {
-    System.out.println("Cetak KRS Semester 1");
-} else if (semester == 2) {
-    System.out.println("Cetak KRS Semester 2");
-} else {
-    System.out.println("Semester tidak valid");
-}
-
+        
+    if (semester == 1) {
+        System.out.println("Cetak KRS Semester 1");
+    } else if (semester == 2) {
+        System.out.println("Cetak KRS Semester 2");
+    } else {
+        System.out.println("Semester tidak valid");
+    }
     }
 }
